1806ce17701613f1726e2fa105053aa287729d74
Sabudh Bahadur Thapa <tsabudh@gmail.com>
2022-11-22 19:10:46 +0545

    pug templates

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const helmet = require('helmet');
@@ -12,14 +13,22 @@ const rateLimit = require('express-rate-limit');
 // const xss = require('xss-clean');
 
 const hpp = require('hpp');
+// const { dirname } = require('path');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
-// 1) GLOBAL MIDDLEWARES
+const viewRouter = require('./routes/viewRoutes');
 
+// 1) GLOBAL MIDDLEWARES
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serving Static Files
+// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, 'public')));
 //Set Security HTTP Headers
 app.use(helmet());
 
@@ -61,9 +70,6 @@ app.use(
   })
 );
 
-// Serving Static Files
-app.use(express.static(`${__dirname}/public`));
-
 app.use('/', (req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
@@ -71,6 +77,8 @@ app.use('/', (req, res, next) => {
   next();
 });
 
+// ROUTES
+app.use('/', viewRouter);
 app.use('/api/v1/tours/', tourRouter);
 app.use('/api/v1/users/', userRouter);
 app.use('/api/v1/reviews/', reviewRouter);
---
+++ routes/viewRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const viewsController = require('../controllers/viewsController');
+
+const router = express.Router();
+
+// ROUTES
+router.get('/', viewsController.getOverview);
+
+router.get('/tour', viewsController.getTour);
+
+module.exports = router;
